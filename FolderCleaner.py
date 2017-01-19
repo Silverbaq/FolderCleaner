e6416150be4d68ad5e01d861982f76fc6c1c8ba6
Steffen Mogensen <feber.sm@gmail.com>
2017-01-19 21:47:20 +0100

    first check in

---
+++ FolderCleaner.py
@@ -0,0 +1,36 @@
+#!/bin/python3.6
+import os
+
+min_size = 10
+folders = {}
+
+def folder_size(path):
+    total = 0
+    for entry in os.scandir(path):
+        if entry.is_file():
+            total += entry.stat().st_size
+        elif entry.is_dir():
+            total += folder_size(entry.path)
+    return total  # in bytes
+
+
+def scan_folder(path='.'):
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            size = folder_size(entry.path) / (1024 * 1024)  # in megabytes
+            if size < min_size:
+                folders[entry.path] = entry.name
+                print('{} : {} Mb'.format(entry.path, round(size, 2)))
+
+
+def move_folders(path='.'):
+    folder_path = path+'/clean_up_folder'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    [os.rename(key, folder_path+'/'+value) for key, value in folders.items()]
+
+
+if __name__ == "__main__":
+    path = '.'
+    scan_folder(path)
+    move_folders(path)
